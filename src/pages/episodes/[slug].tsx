6dce24b7b2af90092cc8f1db20bbc76799f0e61e
Rafael Zorzenon <rafaasimi@hotmail.com>
2021-04-23 10:03:18 -0300

    :sparkles: Fetch episodes from local API

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -45,23 +45,39 @@ export default function Episode({ episode }: EpisodeProps) {
       </div>
 
       <header>
-          <h1>{episode.title}</h1>
-          <span>{episode.members}</span>
-          <span>{episode.publishedAt}</span>
-          <span>{episode.durationAsString}</span>
-        </header>
+        <h1>{episode.title}</h1>
+        <span>{episode.members}</span>
+        <span>{episode.publishedAt}</span>
+        <span>{episode.durationAsString}</span>
+      </header>
 
-        <div
-          className={styles.description}
-          dangerouslySetInnerHTML={{ __html: episode.description }}
-        />
+      <div
+        className={styles.description}
+        dangerouslySetInnerHTML={{ __html: episode.description }}
+      />
     </div>
   );
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get("episodes", {
+    params: {
+      _limit: 2,
+      _sort: "published_at",
+      _order: "desc",
+    },
+  });
+
+  const paths = data.map(episode => {
+    return {
+      params: {
+        slug: episode.id
+      }
+    }
+  })
+
   return {
-    paths: [],
+    paths,
     fallback: "blocking",
   };
 };
